030750bbfd30eb2458552fb8f27eea75cf6b928f
Lucas Novais <lucasno@al.insper.edu.br>
2023-11-16 23:57:40 -0300

    lab7

--- src/main.c
+++ src/main.c
@@ -12,6 +12,11 @@
 /* LCD / LVGL                                                           */
 /************************************************************************/
 
+LV_FONT_DECLARE(dseg70);
+LV_FONT_DECLARE(dseg50);
+LV_FONT_DECLARE(dseg40);
+LV_FONT_DECLARE(dseg30);
+
 #define LV_HOR_RES_MAX          (320)
 #define LV_VER_RES_MAX          (240)
 
@@ -22,6 +27,7 @@ static lv_disp_draw_buf_t disp_buf;
 static lv_color_t buf_1[LV_HOR_RES_MAX * LV_VER_RES_MAX];
 static lv_disp_drv_t disp_drv;          /*A variable to hold the drivers. Must be static or global.*/
 static lv_indev_drv_t indev_drv;
+static lv_obj_t * labelSetValue;
 
 /************************************************************************/
 /* RTOS                                                                 */
@@ -64,6 +70,50 @@ static void event_handler(lv_event_t * e) {
 	}
 }
 
+static void menu_handler(lv_event_t * e) {
+	lv_event_code_t code = lv_event_get_code(e);
+
+	if(code == LV_EVENT_CLICKED) {
+		LV_LOG_USER("Clicked");
+	}
+	else if(code == LV_EVENT_VALUE_CHANGED) {
+		LV_LOG_USER("Toggled");
+	}
+}
+
+static void clk_handler(lv_event_t * e) {
+	lv_event_code_t code = lv_event_get_code(e);
+
+	if(code == LV_EVENT_CLICKED) {
+		LV_LOG_USER("Clicked");
+	}
+	else if(code == LV_EVENT_VALUE_CHANGED) {
+		LV_LOG_USER("Toggled");
+	}
+}
+
+static void up_handler(lv_event_t * e) {
+	lv_event_code_t code = lv_event_get_code(e);
+	char *c;
+	int temp;
+	if(code == LV_EVENT_CLICKED) {
+		c = lv_label_get_text(labelSetValue);
+		temp = atoi(c);
+		lv_label_set_text_fmt(labelSetValue, "%02d", temp + 1);
+	}
+}
+
+static void down_handler(lv_event_t * e) {
+	lv_event_code_t code = lv_event_get_code(e);
+	char *c;
+	int temp;
+	if(code == LV_EVENT_CLICKED) {
+		c = lv_label_get_text(labelSetValue);
+		temp = atoi(c);
+		lv_label_set_text_fmt(labelSetValue, "%02d", temp - 1);
+	}
+}
+
 void lv_ex_btn_1(void) {
 	lv_obj_t * label;
 
@@ -90,10 +140,105 @@ void lv_ex_btn_1(void) {
 /* TASKS                                                                */
 /************************************************************************/
 
-static void task_lcd(void *pvParameters) {
-	int px, py;
+void lv_termostato(void) {
+	
+	static lv_style_t style;
+	lv_style_init(&style);
+	lv_style_set_bg_color(&style, lv_color_black());
+	
+	static  lv_obj_t * labelBtn1;
 
-	lv_ex_btn_1();
+	lv_obj_t * btn1 = lv_btn_create(lv_scr_act());
+	lv_obj_add_event_cb(btn1, event_handler, LV_EVENT_ALL, NULL);
+	lv_obj_align(btn1, LV_ALIGN_BOTTOM_LEFT, 0, 0);
+	lv_obj_add_style(btn1, &style, 0);
+	lv_obj_set_width(btn1, 60);  lv_obj_set_height(btn1, 60);
+	labelBtn1 = lv_label_create(btn1);
+	lv_label_set_text(labelBtn1, "[ " LV_SYMBOL_POWER);
+	lv_obj_center(labelBtn1);
+	
+	
+	static  lv_obj_t * labelBtn2;
+	lv_obj_t * btn2 = lv_btn_create(lv_scr_act());
+	lv_obj_add_event_cb(btn2, menu_handler, LV_EVENT_ALL, NULL);
+	lv_obj_align_to(btn2, btn1, LV_ALIGN_OUT_RIGHT_TOP, 0, 0);
+	lv_obj_add_style(btn2, &style, 0);
+	lv_obj_set_width(btn2, 60);  lv_obj_set_height(btn2, 60);
+
+	labelBtn2 = lv_label_create(btn2);
+	lv_label_set_text(labelBtn2, "| " LV_SYMBOL_LIST);
+	lv_obj_center(labelBtn2);
+	
+	
+	static  lv_obj_t * labelBtn3;
+	lv_obj_t * btn3 = lv_btn_create(lv_scr_act());
+	lv_obj_add_event_cb(btn3, clk_handler, LV_EVENT_ALL, NULL);
+	lv_obj_align_to(btn3, btn2, LV_ALIGN_OUT_RIGHT_TOP, 0, 0);
+	lv_obj_add_style(btn3, &style, 0);
+	
+	lv_obj_set_width(btn3, 60);  lv_obj_set_height(btn3, 60);
+
+
+	labelBtn3 = lv_label_create(btn3);
+	lv_label_set_text(labelBtn3, "| " LV_SYMBOL_SETTINGS " ]");
+	lv_obj_center(labelBtn3);
+	
+	
+	
+	static  lv_obj_t * labelBtn4;
+	lv_obj_t * btn4 = lv_btn_create(lv_scr_act());
+	lv_obj_add_event_cb(btn4, up_handler, LV_EVENT_ALL, NULL);
+	lv_obj_align_to(btn4, btn3, LV_ALIGN_OUT_RIGHT_TOP, 0, 0);
+	lv_obj_add_style(btn4, &style, 0);
+	lv_obj_set_width(btn4, 60);  lv_obj_set_height(btn4, 60);
+
+
+	labelBtn4 = lv_label_create(btn4);
+	lv_label_set_text(labelBtn4, "[ " LV_SYMBOL_UP);
+	lv_obj_center(labelBtn4);
+
+	
+	static  lv_obj_t * labelBtn5;
+	
+
+	lv_obj_t * btn5 = lv_btn_create(lv_scr_act());
+	lv_obj_add_event_cb(btn5, down_handler, LV_EVENT_ALL, NULL);
+	lv_obj_align_to(btn5, btn4, LV_ALIGN_OUT_RIGHT_TOP, 0, 0);
+	lv_obj_add_style(btn5, &style, 0);
+	
+	lv_obj_set_width(btn5, 60);  lv_obj_set_height(btn5, 60);
+
+
+	labelBtn5 = lv_label_create(btn5);
+	lv_label_set_text(labelBtn5, LV_SYMBOL_DOWN " ]");
+	lv_obj_center(labelBtn5);
+	
+	static lv_obj_t * labelFloor;
+	labelFloor = lv_label_create(lv_scr_act());
+	lv_obj_align(labelFloor, LV_ALIGN_LEFT_MID, 35 , -25);
+	lv_obj_set_style_text_font(labelFloor, &dseg70, LV_STATE_DEFAULT);
+	lv_obj_set_style_text_color(labelFloor, lv_color_white(), LV_STATE_DEFAULT);
+	lv_label_set_text_fmt(labelFloor, "%02d", 23);
+	
+	
+	labelSetValue = lv_label_create(lv_scr_act());
+	lv_obj_align(labelSetValue, LV_ALIGN_LEFT_MID, 235 , -35);
+	lv_obj_set_style_text_font(labelSetValue, &dseg50, LV_STATE_DEFAULT);
+	lv_obj_set_style_text_color(labelSetValue, lv_color_white(), LV_STATE_DEFAULT);
+	lv_label_set_text_fmt(labelSetValue, "%02d", 22);
+	
+	
+	static lv_obj_t * labelClock;
+	labelClock = lv_label_create(lv_scr_act());
+	lv_obj_align(labelClock, LV_ALIGN_LEFT_MID, 210 , -95);
+	lv_obj_set_style_text_font(labelClock, &dseg40, LV_STATE_DEFAULT);
+	lv_obj_set_style_text_color(labelClock, lv_color_white(), LV_STATE_DEFAULT);
+	lv_label_set_text_fmt(labelClock, "%s", "17:46");
+}
+
+static void task_lcd(void *pvParameters) {
+	
+	lv_termostato();
 
 	for (;;)  {
 		lv_tick_inc(50);
@@ -170,13 +315,11 @@ void configure_lvgl(void) {
 	disp_drv.ver_res = LV_VER_RES_MAX;      /*Set the vertical resolution in pixels*/
 
 	lv_disp_t * disp;
-	disp = lv_disp_drv_register(&disp_drv); /*Register the driver and save the created display objects*/
 	
 	/* Init input on LVGL */
 	lv_indev_drv_init(&indev_drv);
 	indev_drv.type = LV_INDEV_TYPE_POINTER;
 	indev_drv.read_cb = my_input_read;
-	lv_indev_t * my_indev = lv_indev_drv_register(&indev_drv);
 }
 
 /************************************************************************/
